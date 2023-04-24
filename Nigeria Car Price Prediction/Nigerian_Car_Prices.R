fcb44e568b6eea7f48498d342dd9999bfb6ad90c
CodeWithLouie <fabulouslouie@gmail.com>
2023-04-24 14:16:31 +0100

    bug fixes
    fixed some code errors and improve codes for optimum performance

--- Nigeria Car Price Prediction/Nigerian_Car_Prices.R
+++ Nigeria Car Price Prediction/Nigerian_Car_Prices.R
@@ -14,5 +14,52 @@ car.price %>%
 library(assertive)
 
 car.price %>% 
-  filter(is.na(Condition)) %>% 
+  filter(!is.na(Condition)) %>% ## check for values that are not NA in the cond
+  #condition column
   count()
+
+car.price.not_NA <- car.price %>% 
+  filter(!is.na(Condition)) 
+
+# inspect the new data and check for additional NAs
+
+View(car.price.not_NA)
+
+# Replace NAs in Build with Not Provided
+# call the stingr package 
+
+library(stringr)
+
+car.price.filtered <- car.price.not_NA %>% 
+  mutate(New_Build = str_replace_na(Build, replacement = "Not Available"))
+
+
+car.price.filtered %>% 
+  filter(is.na(New_Build)) %>% 
+  count()
+
+# NAs replaced with not available 
+
+View(car.price.filtered)
+
+# let's see the groupings of car build, transmission, condition, make, & YOM
+
+  car.price.filtered %>% 
+    group_by(New_Build) %>% 
+    count()
+
+    car.price.filtered %>% 
+    group_by(Make) %>% 
+    count() %>% 
+    arrange(desc(n))
+    
+  car.price.filtered %>% 
+    group_by(`Year of manufacture`) %>% 
+    count() %>% 
+    arrange(desc(n))
+  
+  car.price.filtered %>% 
+    group_by(Condition) %>% 
+    count()
+  
+    
